5af058cca4a9db46a8a0b4960695d61d9e44d61e
SumedhaKS <kssumedha24@gmail.com>
2026-02-11 16:16:40 +0530

    pub-sub works

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,58 @@
 import { PubSubManager } from "./PubSubManager";
+import { FinnhubAPI, FinnhubWS } from "@stoqey/finnhub";
+import { createClient, type RedisClientType, } from "redis";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const redisClient: RedisClientType = createClient();
+redisClient.connect();
+
+const finnhubAPI = new FinnhubAPI(process.env.FINNHUB_KEY);
+const finnhubWs = new FinnhubWS()
+
+process.env.DEBUG = "finnhubWs*"
 
 interface User {
     userId: string;
     stock: string;
 }
+
 // users
-const user1: User = {
-    userId: "mahesh",
-    stock: "AAPL",
-}
+const users: User[] = [
+    {
+        userId: "mahesh",
+        stock: "AAPL",
+    },
+    {
+        userId: "suresh",
+        stock: "AAPL"
+    },
+    {
+        userId: "ramesh",
+        stock: "AMZN"
+    }
+]
 
-const user2: User = {
-    userId: "suresh",
-    stock: "AAPL"
-}
-
-const user3: User = {
-    userId: "ramesh",
-    stock: "TESLA"
-}
-
+const stocks: Set<string> = new Set();
 const pubsubManager = PubSubManager.getInstance();
 
 console.log(`Script started`);
 
 // simulating pub sub
-pubsubManager.userSubscribe(user1.userId, user1.stock);
-pubsubManager.userSubscribe(user2.userId, user2.stock);
-pubsubManager.userSubscribe(user3.userId, user3.stock);
+users.forEach((user)=>{
+    pubsubManager.userSubscribe(user.userId, user.stock);
+    if(!stocks.has(user.stock)){
+        stocks.add(user.stock)
+    }
+})
+console.log(stocks);
 
-
-// setInterval(() => {
-//     PubSubManager.getInstance().userSubscribe(Math.random().toString(), "AMZN");
-// }, 5000)
+setInterval(() => {
+    stocks.forEach(async (stock)=>{
+        const quote = await finnhubAPI.getQuote(stock);        
+        redisClient.publish(stock, JSON.stringify(quote));
+    })
+}, 5000)
 
 // fetch current stock price and publish on redis
